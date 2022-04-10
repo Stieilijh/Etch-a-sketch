b058182a52f2dac618c84d880cef70165f7e0439
Stieilijh <saitushar02@gmail.com>
2022-04-10 22:05:04 +0530

    Add inverse button

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ window.onload=function(){
     let grid = document.querySelector("#grid");
     let color="black";
     let invColor="white"; 
-    let slider = document.getElementById("myRange");
+    const slider = document.getElementById("myRange");
     let size = slider.value;
+
+    makeGrid(grid,color,size);
+    
+    const invbtn = document.querySelector("#inverse");
+    invbtn.addEventListener("click",()=>{
+        let temp=color;
+        color=invColor;
+        invColor=temp;
+        grid.style.backgroundColor=invColor;
+        makeGrid(grid,color,size);
+    });
 
     const clearbtn = document.querySelector("#clear");
     clearbtn.addEventListener("click",
@@ -11,32 +22,28 @@ window.onload=function(){
         const tiles = document.querySelectorAll(".cell");
         tiles.forEach((tile)=>tile.style.backgroundColor=invColor);
     });
+
     slider.oninput = function() {
         size=slider.value;
-        clearAll();
-        grid.style.setProperty("--n", size);
-        for (let i = 0; i < size * size; i++) {
-            
-            const div = document.createElement("div");
-            div.classList.add("cell");
-            div.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor=color;
-            });
-            grid.appendChild(div);
+        makeGrid(grid,color,size);
     }
-    }
+    
 
+};
+
+function makeGrid(grid,color,size){
+    clearAll();
     grid.style.setProperty("--n", size);
-
     for (let i = 0; i < size * size; i++) {
         const div = document.createElement("div");
         div.classList.add("cell");
+        div.style.borderColor=color;
         div.addEventListener("mouseover", (e) => {
         e.target.style.backgroundColor=color;
         });
         grid.appendChild(div);
 }
-};
+}
 
 function clearAll(){
     var e = document.querySelector("#grid");
